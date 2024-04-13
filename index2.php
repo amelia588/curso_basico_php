17f539f61037cf99d4b552bad421077aef0347ec
Amelia Martel <1473967@senati.pe>
2024-04-13 11:38:53 -0500

    operaciones

---
+++ index2.php
@@ -0,0 +1,23 @@
+<?php
+//echo "Hola, mamá. Estoy programando con PHPCITO \n";
+// \n se usa para salto de linea
+
+$nombre = "Carlos";
+$apellido = "Martel";
+
+//echo "No, no. Yo me llamo" . " " . $nombre . " " . $apellido . "\n";
+
+// dos comillas se usa para deparar las palabras para que nose vean pegados " ".
+
+//echo "No, no. Yo me llamo $nombre $apellido \n";
+
+$persona = [
+    "Carlos" => 22,
+    "Mr. Michi" => 15,
+    "Juan" => 65
+
+];
+//var_dump me permite inspeccionar
+//var_dump($persona);
+//esto es un comentario
+echo "\n";
